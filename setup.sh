af2fc62dc08fa40c386b51f5245e0c11734c2722
Vladimir Horvat <horvat.vhorvat.vladimir@gmail.com>
2021-05-27 00:24:32 +0200

    Add condition for sh and bash shell

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -F __start_kubectl k' >>~/.bashrc
-
-source ~/.bashrc
+if [ $SHELL == "/bin/sh" ]
+then
+  echo 'alias k=kubectl' >>~/.profile
+  echo 'complete -F __start_kubectl k' >>~/.profile
+  source ~/.profile
+elif [ $SHELL == "/bin/bash" ]
+  echo 'alias k=kubectl' >>~/.bashrc
+  echo 'complete -F __start_kubectl k' >>~/.bashrc
+  source ~/.bashrc
+fi
