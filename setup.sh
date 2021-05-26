97dcb82ceeb5872fe5002527a94f4005da546c23
Vladimir Horvat <horvat.vhorvat.vladimir@gmail.com>
2021-05-27 00:36:29 +0200

    Correct missing then after elif

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ then
   echo 'complete -F __start_kubectl k' >>~/.profile
   source ~/.profile
 elif [ $SHELL == "/bin/bash" ]
+then
   echo 'alias k=kubectl' >>~/.bashrc
   echo 'complete -F __start_kubectl k' >>~/.bashrc
   source ~/.bashrc
